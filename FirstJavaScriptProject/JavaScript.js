32aed7fc414e972e968e670cf6c7045e230372fd
Dudek, Kamil <kamil.dudek@impaqgroup.com>
2015-12-16 11:28:39 +0100

    Commit new project 1

---
+++ FirstJavaScriptProject/JavaScript.js
@@ -0,0 +1,10 @@
+﻿function substitute() {
+    var myValue = document.getElementById('myTextBox').value;
+
+    if (myValue.length === 0) {
+        alert('Please enter a real value in the text box!');
+        return;
+    }
+    var myTitle = document.getElementById('title');
+    myTitle.innerHTML = myValue;
+}
