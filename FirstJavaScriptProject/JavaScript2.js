610ecb20cf4541d22df53dd38fc7347e73ffb6f0
Dudek, Kamil <kamil.dudek@impaqgroup.com>
2015-12-17 08:37:21 +0100

    Commit lesson 8 arrays

--- FirstJavaScriptProject/JavaScript2.js
+++ FirstJavaScriptProject/JavaScript2.js
@@ -1,3 +1,19 @@
 ﻿alert((function() {
     return arguments[0] + arguments[1];
-}) (1, 2));
+})(1, 2));
+
+//tablice
+var array = [];
+
+// array.name 
+array["name"] = 1;
+array[2] = "second";
+array[3] = "four";
+
+//
+var arrays = ["1", "2", "3"];
+
+//Pętla for 
+for (var i in arrays) {
+    document.write("<br />" + arrays[i]);
+}
