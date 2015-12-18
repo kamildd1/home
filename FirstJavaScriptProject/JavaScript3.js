124d1cfdc22ec3579782d4a7f23133c7298913e2
Dudek, Kamil <kamil.dudek@impaqgroup.com>
2015-12-18 08:51:06 +0100

    Commit lesson 12

--- FirstJavaScriptProject/JavaScript3.js
+++ FirstJavaScriptProject/JavaScript3.js
@@ -1,2 +1,18 @@
 ﻿//Zmienna globalna
 myScope = "global"
+
+// Struktura obiektowa
+var structure = {
+    status: "Ready",
+    resorceFunction: function () {
+        document.write("Coś tam");
+    },
+    table: [
+        { locationId: 15, name: "KKK" },
+        { locationId: 16, name: "DDD" }
+]
+}
+
+if (structure.status === "Ready") {
+    document.write(structure.table[0].name);
+}
